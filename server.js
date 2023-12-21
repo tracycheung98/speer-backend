734ae98604436b9af5d3848a4b78624898fa2db3
tracycheung98 <tracycheung98@gmail.com>
2023-12-21 01:56:57 -0500

    Connect Mongodb

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const app = require("./app");
+const mongoose = require("mongoose");
 
+require("dotenv").config();
 const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+
+mongoose
+  .connect(process.env.MONGODB_URI.concat("/prod"))
+  .then(() => {
+    app.listen(PORT, console.log(`Server started on port ${PORT}`));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- tests/tmp.test.js
+++ tests/tmp.test.js
@@ -1,6 +1,16 @@
 const request = require("supertest");
 const app = require("../app");
 
+require("dotenv").config();
+
+beforeEach(async () => {
+  await mongoose.connect(process.env.MONGODB_URI.concat("/prod"));
+});
+
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
 describe("GET /", () => {
     it("should return hi", async () => {
       const res = await request(app).get("/");
